89adae8de0a71bd3c398fa69aea620ab221ae17b
Muhammad Roshan ali <66337567+roshan-ican@users.noreply.github.com>
2022-05-18 12:57:36 +0530

    trying to make the image responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   content: ["./*.html"],
-
   theme: {
     screens: {
       sm: "480px",
       md: "768px",
       lg: "976px",
       xl: "1440px",
+      "2xl": "1535px",
     },
 
     extend: {
